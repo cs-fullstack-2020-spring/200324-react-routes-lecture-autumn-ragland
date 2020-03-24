898a35a35df4ab9865cc1d995a1012b8b9f084b4
autumn-ragland <raglanda998@gmail.com>
2020-03-24 11:35:42 -0500

    routes

--- lecture-app/src/components/AppContainer.jsx
+++ lecture-app/src/components/AppContainer.jsx
@@ -1,21 +1,31 @@
-import React, {Component, Fragment} from 'react';
+import React, { Component, Fragment } from 'react';
 import FilmList from './FilmList'
 import DrinkList from './DrinkList';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 
-class AppContainer extends Component{
-    constructor(props){
+class AppContainer extends Component {
+    constructor(props) {
         super(props);
         this.state = {
 
         }
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <Fragment>
                 <h1>React Routes Lecture</h1>
-                <FilmList/>
-                <DrinkList/>
+                <Router>
+                    <Link to="/">Home</Link>
+                    <Link to="/films">Films</Link>
+                    <Link to="/drinks">Drinks</Link>
+                    <Route path="/films">
+                        <FilmList />
+                    </Route>
+                    <Route path="/drinks">
+                        <DrinkList />
+                    </Route>
+                </Router>
             </Fragment>
         )
     }
